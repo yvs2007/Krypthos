1cba336fd7aeb0d09da8bcf8ce65c035e3eea9d8
yvs2007 <vladimirY2007@gmail.com>
2024-05-10 19:15:28 -0400

    I kinda gave up im just gonna make it in react and type script.

--- First Website/Porject1Script.js
+++ First Website/Porject1Script.js
@@ -1,14 +1,7 @@
-function World() {
-    // Get a reference to the submit button element
-    var submitButton = document.querySelector(".SubmitButton");
-    // Check if the button exists before adding the event listener
-    if (submitButton) {
-        // Add a click event listener to the submit button
-        submitButton.addEventListener("click", function () {
-            // Get the value from the input field
-            var userInput = document.getElementById("UserMessage").value;
-            // Now you can do something with the user input
-            alert("User input: " + userInput);
-        });
-    }
-}
+document.getElementsByClassName('.SubmitButton').addEventListener('click', function() {
+    // When the button is clicked, store the button input in a variable
+    var buttonInput = "Button Clicked";
+    
+    // You can now use the variable buttonInput to do whatever you need with the input
+    console.log('Button Input:', buttonInput);
+});
