d364b724428b6b4d44ca3ae13c85cd2253f243fc
yvs2007 <vladimirY2007@gmail.com>
2024-05-21 21:41:05 -0400

    ,

--- First Website/Porject1Script.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName('.SubmitButton').addEventListener('click', function() {
-    // When the button is clicked, store the button input in a variable
-    var buttonInput = "Button Clicked";
-    
-    // You can now use the variable buttonInput to do whatever you need with the input
-    console.log('Button Input:', buttonInput);
-});
